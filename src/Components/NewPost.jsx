ddbc85077eb6c184ea168d9bf89daadf640e7a3b
Sadeed Ahmad <sadeedahmadch@gmail.com>
2024-11-04 16:37:20 +1000

    added event listner

--- src/Components/NewPost.jsx
+++ src/Components/NewPost.jsx
@@ -1,12 +1,20 @@
+import { useState } from 'react';
 import classes from './NewPost.module.css';
 
 function NewPost() {
-  return (
+    const [enteredBody, setEnteredBody] = useState("");
+
+    function changeBodyHandler (event) {
+        setEnteredBody(event.target.value);
+    }
+    return (
+
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} />
+        <textarea id="body" required rows={3} onChange={changeBodyHandler} />
       </p>
+      <p>{enteredBody}</p>
       <p>
         <label htmlFor="name">Your name</label>
         <input type="text" id="name" required />
