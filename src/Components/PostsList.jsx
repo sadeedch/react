71f2d5e72ac7d66a773784949982b86fd492b981
Sadeed Ahmad <sadeedahmadch@gmail.com>
2024-11-06 17:22:06 +1000

    error handling new posts

--- src/Components/PostsList.jsx
+++ src/Components/PostsList.jsx
@@ -22,12 +22,15 @@ return (
     />
     </Modal>    
     }
+    {posts.length >0 &&  (<ul className= {classes.posts}>
+    {posts.map((post) => <Post author = {post.author}  body = {post.body} ></Post> )}  
+    </ul>)}
+
+    {posts.length === 0 && (<div style={{textAlign: 'center', color: 'white'}}>
+        <h2>There are no Posts yet</h2>
+        <p>Start adding some </p>
+    </div>)}
     
-    <ul className= {classes.posts}>
-    {posts.map((post) => <Post author = {post.author}  body = {post.body} ></Post> )}
-    
-  
-    </ul>
     </>
     )
 
